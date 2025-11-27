d96ef6b5c8ed090a37f687d8e2cf3202c5ccd6f1
Anononion <150773272+Anononion@users.noreply.github.com>
2025-11-28 02:09:11 +0500

    Update GlucoseEntry.cs

--- Models/GlucoseEntry.cs
+++ Models/GlucoseEntry.cs
@@ -3,5 +3,5 @@ namespace DiabetesBot.Models;
 public class GlucoseEntry
 {
     public int Value { get; set; }
-    public DateTime Time { get; set; }
+    public DateTime Timestamp { get; set; }  // ← старое имя
 }
