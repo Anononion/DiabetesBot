5a3cde9e9278cf1bfd22d371fe3bf80597844044
Anononion <150773272+Anononion@users.noreply.github.com>
2025-11-27 17:27:59 +0500

    Update UserState.cs

--- Models/UserState.cs
+++ Models/UserState.cs
@@ -1,26 +1,10 @@
-﻿namespace DiabetesBot.Models;
+using System.Collections.Generic;
 
+namespace DiabetesBot.Models;
 
-public enum UserPhase
-{
-    New,
-    ChoosingLanguage,
-
-    MainMenu,
-
-    // Глюкометрия
-    GlucoseMenu,
-    AwaitGlucoseValue,
-
-    // ХЕ
-    BreadUnits,
-
-    DiabetesSchool,
-    Settings
-}
 public class UserState
 {
     public UserStep Step { get; set; } = UserStep.None;
+
     public Dictionary<string, string> Temp { get; set; } = new();
 }
-
