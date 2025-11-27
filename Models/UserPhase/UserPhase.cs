3f37b4769d6fb8ae6763e6fce2952fd3d9eddbe0
Anononion <150773272+Anononion@users.noreply.github.com>
2025-11-27 17:25:30 +0500

    Create UserPhase.cs

---
+++ Models/UserPhase/UserPhase.cs
@@ -0,0 +1,11 @@
+namespace DiabetesBot.Models;
+
+public enum UserPhase
+{
+    MainMenu = 0,
+    Glucose = 1,
+    BreadUnits = 2,
+    DiabetesSchool = 3,
+    Settings = 4,
+    LanguageChoice = 5
+}
