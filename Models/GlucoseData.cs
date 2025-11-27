b841ca777ff38a03b5517250034001dde00f6db9
Anononion <150773272+Anononion@users.noreply.github.com>
2025-11-28 01:34:08 +0500

    Create GlucoseData.cs

---
+++ Models/GlucoseData.cs
@@ -0,0 +1,8 @@
+namespace DiabetesBot.Models;
+
+public class GlucoseData
+{
+    public string? Type { get; set; }
+    public double? Value { get; set; }
+    public DateTime? Time { get; set; }
+}
