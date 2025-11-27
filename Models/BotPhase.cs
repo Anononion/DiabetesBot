692ddcf77f28607c1f3d7adb417717ca0546f725
Anononion <150773272+Anononion@users.noreply.github.com>
2025-11-27 18:44:50 +0500

    Update BotPhase.cs

--- Models/BotPhase.cs
+++ Models/BotPhase.cs
@@ -2,14 +2,18 @@ public enum BotPhase
 {
     MainMenu,
 
+    // Глюкоза
     Glucose,
     Glucose_ValueInput,
 
+    // Хлебные единицы
     BreadUnits,
     BreadUnits_EnterGrams,
 
+    // Школа диабета
     DiabetesSchool,
 
+    // Настройки
     Settings,
     LanguageChoice
 }
